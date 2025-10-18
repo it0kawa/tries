2ae5ee0af36eb9f7d20bda18d74272f96fe8651b
Tomeu Mestre <tmestret@gmail.com>
2025-10-18 15:30:45 +0200

    First implementation and testing

---
+++ parserNaive.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cctype>
+#include <algorithm>
+#include <cassert>
+#include "naive.hh"
+
+using namespace std;
+
+
+string clean_word(const string& word) {
+    string clean_word;
+    for (char c : word) {
+        if (isalpha(c)) {
+            clean_word += tolower(static_cast<unsigned char>(c));
+        }
+    }
+    return clean_word;
+}
+
+int main()
+{
+    string filename = "test_files/alice_wonderland.txt";
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cerr << "Error: Could not open file " << filename << endl;
+        return 1;
+    }
+
+    Naive<int> trie;
+    string raw_word;
+    int word_position = 1;
+
+    while (file >> raw_word) {
+        string word = clean_word(raw_word);
+        if (!word.empty()) {
+            trie.put(word, word_position);
+        }
+        word_position++;
+    }
+
+
+    // Prova
+    vector<int> alice_positions = trie.get("alice");
+    assert(!alice_positions.empty());
+    assert(alice_positions[0] == 35);
+
+    cout << "Alice trobat a " << alice_positions.size() << " vegades, sa primera a posicio " << alice_positions[0] << "." << endl;
+}
+
